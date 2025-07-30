9090ef27f9858cc9c0d644d116b5f03220ebdc67
Elder-levi <161390233+Elder-levi@users.noreply.github.com>
2025-07-30 17:23:42 -0300

    add funçoes

--- Portifolio/app.js
+++ Portifolio/app.js
@@ -5,6 +5,8 @@ const rootHtml = document.documentElement
 toggleTheme.addEventListener("click", ()=>{
 const curreTheme= rootHtml.getAttribute("data-theme");
 
+curreTheme == "dark" ? rootHtml.setAttribute("data-theme" , "light") : rootHtml.setAttribute("data-theme" , "dark")
+
 toggleTheme.classList.toggle("bi-sun")
 toggleTheme.classList.toggle("bi-moon-stars")
 
